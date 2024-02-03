8b0ca66b6958ebdda360b3dfac08d83897a87cbe
sibkit <sibkit@hotmail.com>
2024-02-03 09:35:58 +0700

    tree_node in rust for create and use tree structures

---
+++ src/lib.rs
@@ -0,0 +1,15 @@
+mod tree_node;
+
+
+
+#[cfg(test)]
+mod tests {
+    use crate::tree_node::TreeNode;
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let mut node = TreeNode::new("str",None);
+        println!("{:?}", node)
+    }
+}
---
+++ src/tree_node.rs
@@ -0,0 +1,64 @@
+use std::cell::RefCell;
+use std::rc::Rc;
+
+#[derive(Debug)]
+pub struct TreeNode<T> {
+    core: Rc<RefCell<NodeCore<T>>>
+}
+
+//pub type TreeNode<T> = Rc<RefCell<NodeCore<T>>>;
+#[derive(Debug)]
+struct NodeCore<T>{
+    value: T,
+    parent: Option<TreeNode<T>>,
+    child_nodes: Vec<TreeNode<T>>
+}
+
+impl <T> TreeNode<T> {
+    pub fn new(value: T, parent: Option<TreeNode<T>>) -> Self {
+
+        let result = TreeNode::<T> {
+            core: Rc::new(RefCell::new(NodeCore {
+                value,
+                parent: parent.clone(),
+                child_nodes: vec![],
+            }))
+        };
+
+        if parent.is_some() {
+            parent.unwrap().core.borrow_mut().child_nodes.push(result.clone());
+        }
+
+
+        return result;
+    }
+    /*
+        fn get_core(&mut self) -> RefMut<NodeCore<T>> {
+            match &mut self.var {
+                RcVar::Rc(rc) => {
+                    rc.borrow_mut()
+                }
+                RcVar::Weak(weak) => {
+                    weak.upgrade().unwrap().borrow_mut()
+                }
+            }
+        }
+    */
+    pub fn add_child(&mut self, node: TreeNode<T>) -> Result<(), String> {
+        node.core.borrow_mut().parent = Some(self.clone());
+        self.core.borrow_mut().child_nodes.push(node);
+        Ok(())
+    }
+
+    pub fn remove_child(&mut self, index: usize) -> Result<TreeNode<T>, String> {
+        Ok(self.core.borrow_mut().child_nodes.remove(index))
+    }
+}
+
+impl <T> Clone for TreeNode<T> {
+    fn clone(&self) -> Self {
+        TreeNode{
+            core: self.core.clone()
+        }
+    }
+}
